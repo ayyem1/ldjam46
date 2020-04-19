83f516a4727cd05534ddf82456281ac897e3e08e
jslawski <jslawski@umich.edu>
2020-04-18 21:56:27 -0400

    Visual health representation and endgame sequence

---
+++ Assets/EndGameSequence.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGameSequence : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject endGameCameraObject;
+    private bool showingEndGame = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.showingEndGame && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("JPS");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            this.GoToEndGameState(other.gameObject);
+        }
+    }
+
+    private void GoToEndGameState(GameObject playerObject)
+    {
+        this.endGameCameraObject.SetActive(true);
+        this.showingEndGame = true;
+    }
+}
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -8,24 +8,52 @@ public class PlayerManager : MonoBehaviour
     private float maxPlayerHealth = 100f;
     [SerializeField, Range(0f, 100f)]
     private float playerHealth;
-    private float healthDecayRate = 0.01f;
+    private float healthDecayRate = 0.1f;
+    private float healthGrowRate = 0.5f; //Note: This must be bigger than healthDecayRate in order to exit the coroutine
 
-    private float collectibleObtainHealthBoost = 10f;
+    private float collectibleObtainHealthBoost = 50f;
+
+    private float maxLightIntensity = 10f;
+    [SerializeField]
+    private Light playerLight;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         this.playerHealth = maxPlayerHealth;
+        this.UpdateLightIntensity();
+    }
+
+    private void UpdateLightIntensity()
+    {
+        this.playerLight.intensity = (this.playerHealth / this.maxPlayerHealth) * this.maxLightIntensity;
     }
 
     private void FixedUpdate()
     {
         this.playerHealth -= this.healthDecayRate;
+        if (this.playerHealth < 0)
+        {
+            this.playerHealth = 0;
+        }
+        this.UpdateLightIntensity();
     }
 
     public void CollectibleObtained()
     {
-        this.playerHealth += this.collectibleObtainHealthBoost;
+        StartCoroutine(this.IncreasePlayerHealth(this.playerHealth + this.collectibleObtainHealthBoost));         
+    }
+
+    public IEnumerator IncreasePlayerHealth(float targetHealth)
+    {
+        while (this.playerHealth < targetHealth)
+        {
+            this.playerHealth += this.healthGrowRate;
+            this.UpdateLightIntensity();
+            yield return null;
+        }
+
         if (this.playerHealth > this.maxPlayerHealth)
         {
             this.playerHealth = this.maxPlayerHealth;
