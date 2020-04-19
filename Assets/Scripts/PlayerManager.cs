202ba20cca617d0124ae11b7e349ea31c5405847
jslawski <jslawski@umich.edu>
2020-04-18 20:40:51 -0400

    Moved scripts, added collectible functionality

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    private float maxPlayerHealth = 100f;
+    [SerializeField, Range(0f, 100f)]
+    private float playerHealth;
+    private float healthDecayRate = 0.01f;
+
+    private float collectibleObtainHealthBoost = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.playerHealth = maxPlayerHealth;
+    }
+
+    private void FixedUpdate()
+    {
+        this.playerHealth -= this.healthDecayRate;
+    }
+
+    public void CollectibleObtained()
+    {
+        this.playerHealth += this.collectibleObtainHealthBoost;
+        if (this.playerHealth > this.maxPlayerHealth)
+        {
+            this.playerHealth = this.maxPlayerHealth;
+        }
+    }
+}
