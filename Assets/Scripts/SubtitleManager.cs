c5314d74d6afe2fda8c64f82b12c7fdd8a3fe5a6
ruddgasm <45502739+ruddgasm@users.noreply.github.com>
2020-04-20 04:43:59 -0700

    Revert "Revert "Graceful handling of multiple messages displaying in quick succession""
    This reverts commit 1ea421e7e485861ae729fc35a9ace3766819be85.

--- Assets/Scripts/SubtitleManager.cs
+++ Assets/Scripts/SubtitleManager.cs
@@ -14,6 +14,8 @@ public class SubtitleManager : MonoBehaviour
     [SerializeField]
     private Text titleText;
 
+    Coroutine currentlyDisplayingSubtitle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,9 +29,16 @@ public class SubtitleManager : MonoBehaviour
 
     public void DisplaySubtitle(string subtitleText)
     {
-        this.messageText.text = subtitleText;
-
-        StartCoroutine(this.CycleFade(this.messageText));
+        if (this.currentlyDisplayingSubtitle == null)
+        {
+            this.messageText.text = subtitleText;
+            this.currentlyDisplayingSubtitle = StartCoroutine(this.CycleFade(this.messageText));
+        }
+        else
+        {
+            StopAllCoroutines();
+            StartCoroutine(this.StartNewSubtitle(subtitleText));
+        }
     }
 
     public void DisplayTitle()
@@ -39,18 +48,37 @@ public class SubtitleManager : MonoBehaviour
 
     private IEnumerator CycleFade(Text textObject)
     {
-        for (float i = 0; i < 1; i += Time.fixedDeltaTime * this.fadeRate)
+        for (float i = 0; i < 1; i += Time.deltaTime * this.fadeRate)
         {
             textObject.color = new Color(textObject.color.r, textObject.color.b, textObject.color.g, i);
             yield return null;
         }
-        
-        yield return new WaitForSeconds(this.totalDisplayTime);
 
+        for (float i = 0; i < this.totalDisplayTime; i += Time.deltaTime)
+        {
+            yield return null;
+        }
+        
         for (float i = 1; i >= 0; i -= Time.fixedDeltaTime * this.fadeRate)
         {
             textObject.color = new Color(textObject.color.r, textObject.color.b, textObject.color.g, i);
             yield return null;
         }
+
+        this.currentlyDisplayingSubtitle = null;
+    }
+
+    private IEnumerator StartNewSubtitle(string newSubtitle)
+    {
+        Debug.LogError("I'm here!!");
+
+        for (float i = this.messageText.color.a; i >= 0; i -= Time.deltaTime * 3.0f)
+        {
+            this.messageText.color = new Color(messageText.color.r, messageText.color.b, messageText.color.g, i);
+            yield return null;
+        }
+
+        this.messageText.text = newSubtitle;
+        this.currentlyDisplayingSubtitle = StartCoroutine(this.CycleFade(this.messageText));
     }
 }
